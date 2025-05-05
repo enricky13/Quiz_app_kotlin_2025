eeff1cc0cd5aae2f001b5cb6de7888087b8020bc
Enrique Cruz <enriquecruz@Pursuits-MacBook-Pro.local>
2025-05-05 16:21:51 -0400

    questions_and_answers_shown_correctly

--- app/src/main/java/com/example/quiz_game_compose/QuizUIView/QuestionDisplayScreen.kt
+++ app/src/main/java/com/example/quiz_game_compose/QuizUIView/QuestionDisplayScreen.kt
@@ -1,5 +1,8 @@
 package com.example.quiz_game_compose.QuizUIView
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,18 +13,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun QuestionScreen(viewModel: QuestionDisplayViewModel){
     val uiState by viewModel.uiState.collectAsState()
+    Column {
+        when (uiState) {
+            is QuizUiState.Loading -> {} // Do something while loading
+            is QuizUiState.Success -> {
+                val response = (uiState as QuizUiState.Success).data.results
+                DisplayQuestion(response[viewModel.currentQuestionIndex].question)
+                DisplayAnswers(
+                    viewModel.getShuffledAnswers(
+                        response[viewModel.currentQuestionIndex].correctAnswer,
+                        response[viewModel.currentQuestionIndex].incorrectAnswers
+                    ),
+                    viewModel
+                )
+            }
 
-    when(uiState){
-        is QuizUiState.Loading -> {} // Do something while loading
-        is QuizUiState.Success -> {}
-        is QuizUiState.Error -> {}
+            is QuizUiState.Error -> {}
+        }
     }
 }
 @Composable
@@ -40,16 +57,29 @@ fun DisplayQuestion(question: String) {
 }
 
 @Composable
-fun DisplayAnswers(correctAnswer: String, incorrectAnswers: List<String>){
-
+fun DisplayAnswers(answers: List<String>, viewModel: QuestionDisplayViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Log.d("FINDME", "List size: ${answers.size}")
+        answers.forEach { answer ->
+            Log.d("FINDME", answer)
+            AnswerTemplate(answer, viewModel.answerOnClick(answer))
+        }
+    }
 }
 
 @Composable
-fun AnswerTemplate(answer: String){
+fun AnswerTemplate(answer: String, onClickListener: () -> Unit){
     Button(
-        onClick = {/*Put on click logic for selecting wrong and right answer*/},
-        modifier = Modifier.height(5.dp)
-            .width(5.dp)
+        onClick = onClickListener,
+        modifier = Modifier
+            .height(40.dp)
+            .fillMaxWidth()
     ) {
         Text(answer)
     }
--- app/src/main/java/com/example/quiz_game_compose/QuizUIView/QuestionDisplayViewModel.kt
+++ app/src/main/java/com/example/quiz_game_compose/QuizUIView/QuestionDisplayViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.quiz_game_compose.QuizUIView
 
 import android.util.Log
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.quiz_game_compose.QuizRepository.QuizApi
@@ -8,19 +9,26 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class QuestionDisplayViewModel(
     private val quizApi: QuizApi
 ): ViewModel() {
     private val _uiState = MutableStateFlow<QuizUiState>(QuizUiState.Loading)
     val uiState: StateFlow<QuizUiState> = _uiState.asStateFlow()
+
+    var currentQuestionIndex by mutableIntStateOf(0) // 0 - 9 limit
+    private var correctAnswersCheck = 0
+    private var incorrectAnswersCheck = 0
     private val FINDME = "ENRCRZ"
 
     init {
         retrieveQuestions(10, 9, "easy", "multiple")
     }
 
-    private fun retrieveQuestions(amount: Int, category: Int, difficulty: String, type: String){
+    private fun retrieveQuestions(amount: Int, category: Int, difficulty: String, type: String) {
         /**
          * Instead of writing how to get back data from a retrofit service here, look at the following lines in the
          * uncommented code and see how you are connecting the viewmodel UIState to a success or error response
@@ -43,6 +51,28 @@ class QuestionDisplayViewModel(
                 _uiState.value = QuizUiState.Error(e.localizedMessage ?: "Unknown Error")
                 Log.d(FINDME, "Exception: ${e.message ?: "Unknown error"}")
             }
+        }
+    }
+
+    fun getShuffledAnswers(correctAnswer: String, incorrectAnswers: List<String>): List<String> {
+        /**
+         * If you do (correctAnswer + incorrectAnswer) you will have the incorrectAnswers added to the string
+         * and get back 1 string with all the answers.
+         */
+        return (incorrectAnswers + correctAnswer).shuffled()
+    }
+
+    fun answerOnClick(answer: String): () -> Unit {
+        return {
+            if (answer == (uiState.value as QuizUiState.Success).data.results[currentQuestionIndex].correctAnswer) {
+                Log.d(FINDME, "Correct answer: $answer")
+                correctAnswersCheck++
+            }
+            else{
+                Log.d(FINDME, "Incorrect Answer: $answer")
+                incorrectAnswersCheck++
+            }
+            currentQuestionIndex++
         }
     }
 }
