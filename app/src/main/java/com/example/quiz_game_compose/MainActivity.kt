bf13771e8de7ebbf677f4d3781b650d8c68397e7
Enrique Cruz <enriquecruz@Pursuits-MacBook-Pro.local>
2025-05-08 17:04:40 -0400

    button_to_reset_added

--- app/src/main/java/com/example/quiz_game_compose/MainActivity.kt
+++ app/src/main/java/com/example/quiz_game_compose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.quiz_game_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.quiz_game_compose.QuizUIView.QuestionScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
